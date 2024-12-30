30abfb8826d7cb24886d8fa4e9e7bb347e5b354f
Chris Helder <46414358+TheDude53@users.noreply.github.com>
2024-12-29 20:52:26 -0600

    Improve Glitch log verbosity

--- glitchSync.js
+++ glitchSync.js
@@ -4,9 +4,9 @@ console.log("Syncing from GitHub");
 require("child_process").spawnSync("git", ["clone", "https://github.com/TheDude53/DudeBox.git"], {stdio: "inherit"});
 
 // Move cloned files back into working directory
-require("child_process").execSync("mv", ["-f", "./DudeBox/*", "."]);
+require("child_process").spawnSync("mv", ["-f", "./DudeBox/*", "."], {stdio: "inherit"});
 // Delete old clone folder
-require("child_process").execSync("rm", ["-rf", "./DudeBox"]);
+require("child_process").spawnSync("rm", ["-rf", "./DudeBox"], {stdio: "inherit"});
 
 // Start the server
 require("./server.js");
