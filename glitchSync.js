e9c89cd2761c2e98dd8f3ecff3b90ac0dd51da6a
Chris Helder <46414358+TheDude53@users.noreply.github.com>
2024-12-29 21:20:57 -0600

    Smooth out sync process

--- glitchSync.js
+++ glitchSync.js
@@ -1,12 +1,12 @@
 "use strict";
 
 console.log("Syncing from GitHub");
-require("child_process").spawnSync("git", ["clone", "https://github.com/TheDude53/DudeBox.git"], {stdio: "inherit"});
+require("child_process").execSync("git clone https://github.com/TheDude53/DudeBox.git");
 
 // Move cloned files back into working directory
-require("child_process").spawnSync("mv", ["-f", "./DudeBox/*", "."], {stdio: "inherit"});
+require("child_process").execSync("cp -rf ./DudeBox/* .");
 // Delete old clone folder
-require("child_process").spawnSync("rm", ["-rf", "./DudeBox"], {stdio: "inherit"});
+require("child_process").execSync("rm -rf ./DudeBox");
 
 // Start the server
 require("./server.js");
